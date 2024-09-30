019949d380c5824c78424af8c837fc9dc025bac8
Agung Wicaksono <gungcaksono.id@gmail.com>
2024-09-30 23:08:32 +0800

    feat: enrich data gathering

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,36 +1,189 @@
-import { Column, DataSource, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as newGuid } from 'uuid';
+import { Column, DataSource, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'Animes' })
 export class Anime {
-  constructor(id: string, mal_id: number, title: string, synopsis: string, genres: string) {
-    this.id = id;
+  constructor(
+    mal_id: number,
+    title: string,
+    synopsis: string,
+    start_date: number,
+    end_date: number,
+    nsfw: string,
+    status: string,
+    num_episodes: number,
+    start_year: number,
+    start_season: string,
+    source: string,
+    rating: string
+  ) {
+    this.guid = newGuid();
     this.mal_id = mal_id;
     this.title = title;
     this.synopsis = synopsis;
-    this.genres = genres;
+    this.start_date = start_date;
+    this.end_date = end_date;
+    this.nsfw = nsfw;
+    this.status = status;
+    this.num_episodes = num_episodes;
+    this.start_year = start_year;
+    this.start_season = start_season;
+    this.source = source;
+    this.rating = rating;
   }
 
-  @PrimaryColumn()
-  id: string;
+  @PrimaryGeneratedColumn({ name: 'Id' })
+  id: number;
 
-  @Column()
+  @Column({ name: 'Guid' })
+  guid: string;
+
+  @Column({ name: 'MalId' })
   mal_id: number;
 
-  @Column()
+  @Column({ name: 'Title' })
   title: string;
 
-  @Column()
+  @Column({ name: 'Synopsis' })
   synopsis: string;
 
-  @Column()
-  genres: string;
+  @OneToMany(() => AnimeGenre, (genre) => genre.anime)
+  genres: AnimeGenre[];
+
+  @Column({ name: 'StartDate' })
+  start_date: number;
+
+  @Column({ name: 'EndDate' })
+  end_date: number;
+
+  @Column({ name: 'Nsfw' })
+  nsfw: string;
+
+  @Column({ name: 'Status' })
+  status: string;
+
+  @Column({ name: 'NumberOfEpisodes' })
+  num_episodes: number;
+
+  @Column({ name: 'StartYear' })
+  start_year: number;
+
+  @Column({ name: 'StartSeason' })
+  start_season: string;
+
+  @Column({ name: 'Source' })
+  source: string;
+
+  @Column({ name: 'Rating' })
+  rating: string;
+
+  @OneToMany(() => AnimeStudio, (studio) => studio.anime)
+  studios: AnimeStudio[];
+
+  @OneToMany(() => RelatedAnime, (related_anime) => related_anime.anime)
+  related_anime: RelatedAnime[];
+
+  @OneToMany(() => AnimeRecommendation, (recommendation) => recommendation.anime)
+  recommendations: AnimeRecommendation[];
+}
+
+@Entity({ name: 'RelatedAnimes' })
+export class RelatedAnime {
+  constructor(anime: Anime, mal_id: number, relation_type: string) {
+    this.guid = newGuid();
+    this.anime = anime;
+    this.mal_id = mal_id;
+    this.relation_type = relation_type;
+  }
+
+  @PrimaryGeneratedColumn({ name: 'Id' })
+  id: number;
+
+  @Column({ name: 'Guid' })
+  guid: string;
+
+  @ManyToOne(() => Anime, (anime) => anime.related_anime)
+  anime: Anime;
+
+  @Column({ name: 'MalId' })
+  mal_id: number;
+
+  @Column({ name: 'RelationType' })
+  relation_type: string;
+}
+
+@Entity({ name: 'AnimeRecommendations' })
+export class AnimeRecommendation {
+  constructor(anime: Anime, mal_id: number, num_recommendations: number) {
+    this.guid = newGuid();
+    this.anime = anime;
+    this.mal_id = mal_id;
+    this.num_recommendations = num_recommendations;
+  }
+
+  @PrimaryGeneratedColumn({ name: 'Id' })
+  id: number;
+
+  @Column({ name: 'Guid' })
+  guid: string;
+
+  @ManyToOne(() => Anime, (anime) => anime.recommendations)
+  anime: Anime;
+
+  @Column({ name: 'MalId' })
+  mal_id: number;
+
+  @Column({ name: 'NumberOfRecommendations' })
+  num_recommendations: number;
+}
+
+@Entity({ name: 'AnimeStudios' })
+export class AnimeStudio {
+  constructor(anime: Anime, name: string) {
+    this.guid = newGuid();
+    this.anime = anime;
+    this.name = name;
+  }
+
+  @PrimaryGeneratedColumn({ name: 'Id' })
+  id: number;
+
+  @Column({ name: 'Guid' })
+  guid: string;
+
+  @ManyToOne(() => Anime, (anime) => anime.studios)
+  anime: Anime;
+
+  @Column({ name: 'Name' })
+  name: string;
+}
+
+@Entity({ name: 'AnimeGenres' })
+export class AnimeGenre {
+  constructor(anime: Anime, name: string) {
+    this.guid = newGuid();
+    this.anime = anime;
+    this.name = name;
+  }
+
+  @PrimaryGeneratedColumn({ name: 'Id' })
+  id: number;
+
+  @Column({ name: 'Guid' })
+  guid: string;
+
+  @ManyToOne(() => Anime, (anime) => anime.studios)
+  anime: Anime;
+
+  @Column({ name: 'Name' })
+  name: string;
 }
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: 'data-source.sqlite',
+  database: 'data_source_ver_2.sqlite',
   synchronize: true,
-  entities: [Anime],
+  entities: [Anime, RelatedAnime, AnimeRecommendation, AnimeGenre, AnimeStudio],
   subscribers: [],
   migrations: [],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { v4 as newGuid } from 'uuid';
 import http from './axios';
-import { Anime, AppDataSource } from './dataSource';
+import { Anime, AnimeGenre, AnimeRecommendation, AnimeStudio, AppDataSource, RelatedAnime } from './dataSource';
+import moment from 'moment';
 
 let x = 0;
 
@@ -8,32 +9,85 @@ async function getAndSaveDetailAnime(id: number): Promise<number> {
   x++;
   let statusCode: number = 200;
 
-  const response = await http.get(`/anime/${id}`, { params: { fields: 'title,synopsis,genres' } }).catch((error) => {
-    if (error.response) {
-      console.error(`${id} not saved (${error.response.status}) - (req: ${x})`);
-      statusCode = error.response.status;
-    } else {
-      console.error(`${error} - (req: ${x})`);
-      statusCode = 0;
-    }
-  });
+  const response = await http
+    .get(`/anime/${id}`, {
+      params: {
+        fields:
+          'title,synopsis,genres,start_date,end_date,nsfw,status,num_episodes,start_season,source,rating,studios,related_anime,recommendations',
+      },
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.error(`${id} not saved (${error.response.status}) - (req: ${x})`);
+        statusCode = error.response.status;
+      } else {
+        console.error(`${error} - (req: ${x})`);
+        statusCode = 0;
+      }
+    });
 
   if (response && statusCode == 200) {
-    const data = {
-      id: newGuid(),
-      mal_id: response.data.id,
-      title: response.data.title,
-      synopsis: response.data.synopsis.replaceAll('\n', '').replaceAll('\r', ''),
-      genres: '',
-    };
+    const start_date = moment.utc(response.data.start_date);
+    const end_date = moment.utc(response.data.end_date);
 
+    const anime = new Anime(
+      response.data.id,
+      response.data.title,
+      response.data.synopsis,
+      start_date.unix(),
+      end_date.unix(),
+      response.data.nsfw,
+      response.data.status,
+      response.data.num_episodes,
+      response.data.start_season.year,
+      response.data.start_season.season,
+      response.data.source,
+      response.data.rating
+    );
+
+    await AppDataSource.manager.save(anime);
+
+    let genres: AnimeGenre[] = [];
     if (response.data.genres) {
-      data.genres = response.data.genres
-        .map((genre: { name: string }) => genre.name.toLowerCase().replaceAll(' ', '_'))
-        .join(',');
+      genres = response.data.genres.map((genre: { name: string }): AnimeGenre => {
+        const genreName = genre.name.toLowerCase().replaceAll(' ', '_');
+        return new AnimeGenre(anime, genreName);
+      });
+
+      await AppDataSource.manager.save(genres);
     }
 
-    await AppDataSource.manager.save(AppDataSource.manager.create(Anime, data));
+    let related_anime: RelatedAnime[] = [];
+    if (response.data.related_anime) {
+      related_anime = response.data.related_anime.map(
+        (related_anime: { node: { id: number }; relation_type: string }): RelatedAnime => {
+          return new RelatedAnime(anime, related_anime.node.id, related_anime.relation_type);
+        }
+      );
+
+      await AppDataSource.manager.save(related_anime);
+    }
+
+    let recommendations: AnimeRecommendation[] = [];
+    if (response.data.recommendations) {
+      recommendations = response.data.recommendations.map(
+        (recommendations: { node: { id: number }; num_recommendations: number }): AnimeRecommendation => {
+          return new AnimeRecommendation(anime, recommendations.node.id, recommendations.num_recommendations);
+        }
+      );
+
+      await AppDataSource.manager.save(recommendations);
+    }
+
+    let studios: AnimeStudio[] = [];
+    if (response.data.studios) {
+      studios = response.data.studios.map((studios: { name: string }): AnimeStudio => {
+        return new AnimeStudio(anime, studios.name);
+      });
+
+      await AppDataSource.manager.save(studios);
+    }
+
     console.log(`${id} saved - (req: ${x})`);
   }
 
